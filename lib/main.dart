083713317f39e23295399fb67d4d711c49565c19
soojubm <soojubm@gmail.com>
2025-02-25 11:19:38 +0900

    move theme config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/login_screen.dart';
+import 'package:flutter_application_1/theme.dart';
 import 'screens/splash_screen.dart';
 import 'screens/settings_screen.dart';
+
+// guides
+// 1차에서는 tokenize 하지 않고 themeData에서 바로 사용해 본다.
+// colorScheme이이 MD3 방식.
 
 void main() {
   runApp(const MyApp());
@@ -14,11 +19,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.green,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
 
       initialRoute: '/',
---
+++ lib/theme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF000000);
+
+ThemeData lightTheme = ThemeData(
+  colorScheme: ColorScheme.light(
+    primary: primaryColor,
+    secondary: Color(0xFFFFC107),
+    // background: Color(0xFFFFFFFF),
+    surface: Color(0xFFF5F5F5),
+    error: Color.fromARGB(255, 202, 68, 95),
+    onPrimary: Colors.white,
+    onSecondary: Colors.black,
+    // onBackground: Colors.black,
+    onSurface: Colors.black,
+    onError: Colors.white,
+  ),
+  textTheme: TextTheme(
+      // headline1: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      // bodyText1: TextStyle(fontSize: 16),
+      ),
+  buttonTheme: ButtonThemeData(buttonColor: primaryColor),
+  useMaterial3: true,
+);
+
+ThemeData darkTheme = ThemeData(
+  colorScheme: ColorScheme.dark(
+    // 다크 모드 색상
+    primary: Colors.blueGrey,
+    secondary: Colors.amber,
+    background: Colors.black,
+    surface: Colors.grey[800]!,
+    error: Colors.redAccent,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    onBackground: Colors.white,
+    onSurface: Colors.white,
+    onError: Colors.white,
+  ),
+);
