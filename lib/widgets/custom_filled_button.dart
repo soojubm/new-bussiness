3a965c48cff3b2c1f0ae434e949d403582f85838
soojubm <soojubm@gmail.com>
2025-02-25 10:42:56 +0900

    add CustomFilledButton

---
+++ lib/widgets/custom_filled_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+const double fontSize = 14.0;
+const double borderRadius = 4.0;
+
+class CustomFilledButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomFilledButton({
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: onPressed,
+      style: _buttonStyle(),
+      child: Text(text),
+    );
+  }
+
+  ButtonStyle _buttonStyle() {
+    return ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(Colors.blue),
+      minimumSize: MaterialStateProperty.all(Size(120.0, 48.0)),
+      textStyle: MaterialStateProperty.all(
+        TextStyle(
+          fontSize: fontSize,
+          // fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+      padding: MaterialStateProperty.all(
+        EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+      ),
+      shape: MaterialStateProperty.all(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
+      elevation: MaterialStateProperty.all(2.0),
+    );
+  }
+}
