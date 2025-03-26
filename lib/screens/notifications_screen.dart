a064dd33f1449195a07e4f213a34519f6d258edd
soojubm <soojubm@gmail.com>
2025-03-26 16:19:50 +0900

    refactor notifications screen

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -17,30 +17,70 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  // backgroundColor: Color,
-                  child: const Text('CJ'),
-                ),
-                title: Text('이창준'),
-                subtitle: Text('벤치마킹 참고자료 공유 드라이브에 올려놨어요.'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.notifications_sharp),
-                title: Text('Notification 2'),
-                subtitle: Text('This is a notification'),
-              ),
-            ),
-          ],
+      body: ListView(children: [
+        ListTile(
+          leading: CircleAvatar(
+            // backgroundColor: Color,
+            child: const Text('CJ'),
+          ),
+          title: Text('이창준'),
+          subtitle: Text('벤치마킹 참고자료 공유 드라이브에 올려놨어요.'),
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SettingsTestScreen()));
+            print('');
+          },
         ),
+        ListTile(
+          leading: Icon(Icons.notifications_sharp),
+          title: Text('Notification 2'),
+          subtitle: Text('This is a notification'),
+        ),
+        // Card(
+        //   child: ListTile(
+        //     leading: Icon(Icons.notifications_sharp),
+        //     title: Text('Notification 2'),
+        //     subtitle: Text('This is a notification'),
+        //   ),
+        // ),
+      ]),
+    );
+  }
+}
+
+class SettingsTestScreen extends StatefulWidget {
+  @override
+  _SettingsTestScreenState createState() => _SettingsTestScreenState();
+}
+
+class _SettingsTestScreenState extends State<SettingsTestScreen> {
+  bool _emailNotifications = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
       ),
+      body: ListView(children: [
+        SwitchListTile(
+          title: const Text('이메일 알림'),
+          value: _emailNotifications,
+          onChanged: (bool value) {
+            setState(() {
+              _emailNotifications = value;
+            });
+          },
+        ),
+        SwitchListTile(
+          title: Text('test'),
+          value: _emailNotifications,
+          onChanged: (bool value) {
+            setState(() {
+              _emailNotifications = value;
+            });
+          },
+        ),
+      ]),
     );
   }
 }
